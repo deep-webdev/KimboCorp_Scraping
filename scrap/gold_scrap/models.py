4e4a2f25995b9fe984bf98fdd8cf62366894f0e7
Deep Panchal <deeppanchal2410@gmail.com>
2022-07-03 17:05:54 +0530

    feat: changes related to html added also added stock field in extracted prod

--- scrap/gold_scrap/models.py
+++ scrap/gold_scrap/models.py
@@ -14,6 +14,7 @@ class Extracted(models.Model):
     product_url = models.CharField(max_length=50)
     supplier_name = models.CharField(max_length=20)
     supplier_country = models.CharField(max_length=20)
+    stock = models.CharField(max_length=10)
     
     def __str__(self):
         return self.supplier_name
